472cbac160ecb3dc075cd445dd3766c07941d42f
Matias Kochman <matiaskochman@gmail.com>
2023-07-31 09:40:26 -0300

    feat: reading the logfile and controlling read errors

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const logPath = "data/qgames.log"; // Change this to the correct path
+
+try {
+  // Try to read the file
+  const logText = fs.readFileSync(logPath, "utf-8");
+  // You can process the file content as desired here
+} catch (error) {
+  // If there is an error (such as the file not existing), it will be caught here
+  console.error(`Error reading the file: ${error.message}`);
+}
